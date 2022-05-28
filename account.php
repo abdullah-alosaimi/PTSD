95d6e8b984c0e66eaa0013d331bdb1cef5d52cef
abdullah-alosaimi <abdullah.alosaimi@ccse.ku.edu.kw>
2022-05-29 01:44:57 +0300

    lol

--- account.php
+++ account.php
@@ -16,38 +16,7 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light sticky-top rounded-bottom rounded-3" style="background-color: #bce0fa;">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="./index.html">
-                <img src="./favicon.svg" alt="Icon" width="50" height="60">
-            </a>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 fw-bolder">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="./index.html">Home</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Account
-                </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="./requests.html">Manage requests</a></li>
-                            <li><a class="dropdown-item" href="./offerbook.html">Make an offer</a></li>
-                            <li><a class="dropdown-item" href="./account.html">Account page</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Sign out</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+    <?php  include './header.php'; ?>
 
 
     <div class="container-fluid">
