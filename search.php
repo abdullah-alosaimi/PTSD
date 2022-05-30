e70ef541f5c4e8c32ee561c7a1a8ef5903869405
abdullah-alosaimi <abdullah.alosaimi@ccse.ku.edu.kw>
2022-05-30 11:32:30 +0300

    ..

--- search.php
+++ search.php
@@ -6,6 +6,8 @@
 
     <div class="row row-cols-1 row-cols-md-4 g-4 mt-2  ms-5">
         <?php 
+            
+
             for($i=0; $i < 10; $i++){
                 $url = './img/java-how-to.png';
                 $offer_no = 99;
