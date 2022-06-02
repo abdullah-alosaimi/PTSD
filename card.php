ca4fec02a4a54dc14e91aec12728c3ec60fe7696
abdullah-alosaimi <abdullah.alosaimi@ccse.ku.edu.kw>
2022-06-02 20:03:12 +0300

    lol

--- card.php
+++ card.php
@@ -4,7 +4,11 @@
         <div class="card-body">
             <h5 class="card-title"><?php echo $title ?></h5>
             <p class="card-text">Number of Offers: [<?php echo $offer_no ?>] <br> Number of requests: [<?php echo $req_no ?>] </p>
-            <a href="#" class="btn btn-primary">Request the book</a>
+            <form action="" method="post">
+                <input type="hidden" id="bookid" name="bookid" class="none" value="<?php echo $id ?>">
+                <button type="submit" class="btn btn-primary">Request the book</button>
+            </form>
+            
         </div>
     </div>
 </div>
