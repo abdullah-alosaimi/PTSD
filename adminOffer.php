113bf0198a946da77ff626f6715e9ff7041b289d
abdullah-alosaimi <abdullah.alosaimi@ccse.ku.edu.kw>
2022-06-02 22:31:03 +0300

    kk

---
+++ adminOffer.php
@@ -0,0 +1,73 @@
+<?php
+    include 'init.php';
+    if(!isset($_SESSION['admin'])){
+        header("location: index.php");
+    }
+
+    include 'adminHeader.php';
+?>
+
+<div class="container-fluid">
+    <div class="row p-2">
+            <h1>All Books: </h1>
+            <table class="table table-info table-striped shadow col-12 h-50 w-100 align-middle p-3 border border-dark rounded">
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            Offer ID
+                        </th>
+                        <th scope="col">
+                            Date
+                        </th>
+                        <th scope="col">
+                           Book ID
+                        </th>
+                        <th scope="col">
+                            User ID
+                        </th>
+                        <th scope="col" class="text-center">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        include 'db.php';
+
+                        $sql = "SELECT * FROM `offers`";
+                        $result = mysqli_query($db, $sql);
+
+                        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                            $id = $row['id'];
+                            $date = $row['date'];
+                            $bid = $row['bid'];
+                            $uid = $row['uid'];
+
+                            echo "
+                                    <tr>
+                                <th scope=\"row\">
+                                    $id
+                                </th>
+                                <td>
+                                    $date
+                                </td>
+                                <td>
+                                    $bid
+                                </td>
+                                <td>
+                                    $uid
+                                </td>
+                                <td class=\"text-center\">
+                                    <form method=\"POST\" action=\"./deleteOffer.php\">
+                                        <input type=\"hidden\" value=\"$id\">
+                                        <button type=\"button\" class=\"btn btn-danger\">DELETE</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            ";
+                        }
+                    ?>
+                </tbody>
+            </table>
+    </div>
+</div>
